4ef075009c96af2f96e7a1d6fa9c4e3b9ae5e915
catperez81 <catperez81@gmail.com>
2018-08-29 20:15:55 -0700

    added describe for restart

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,11 +4,31 @@ import {restartGame, makeGuess, generalAuralUpdate} from './actions';
 
 describe('Reducer', () => {
 	it('Should set the initial state', () => {
+		// why pass through undefined? and type unknown? //
 		const state =  reducer(undefined, {type: '_UNKNOWN'});
 
 		expect(state.guesses).toEqual({[]});
 		expect(state.feedback).toEqual('Make a guess!');
 		expect(state.auralStatus).toEqual('');
-
+		//never heard of the below method around greater than or equal to - is this just redux? //
+		expect(state.correctAnswer).toBeGreaterThanOrEqual('0');
+		expect(state.correctAnswer).toBeLessThanOrEqual('100');
 	});
+});
+
+describe('RestartGame', () => {
+	it('Should start a new game', () => {
+	// set initial state to run this test //
+		let state = {
+			guesses: [1,2,3,4,5],
+			feedback: 'You got it!', 
+			correctAnswer: 5
+		};
+
+		const correctAnswer = 20;
+
+		expect(state.guesses).toEqual([]);
+		expect(state.feedback).toEqual('Make a guess!');
+		expect(state.auralStatus).toEqual('');
+		expect(state.correctAnswer).toEqual(correctAnswer);
 });
