725b22affc57b993a6fdbe83d5a935afac1c9057
catperez81 <catperez81@gmail.com>
2018-08-29 20:00:34 -0700

    working on reducer test

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,9 +4,11 @@ import {restartGame, makeGuess, generalAuralUpdate} from './actions';
 
 describe('Reducer', () => {
 	it('Should set the initial state', () => {
-		let state =  reducer(undefined, {type: '_UNKNOWN'});
+		const state =  reducer(undefined, {type: '_UNKNOWN'});
 
-		expect(state).toEqual({[]});
-		
+		expect(state.guesses).toEqual({[]});
+		expect(state.feedback).toEqual('Make a guess!');
+		expect(state.auralStatus).toEqual('');
+
 	});
 });
